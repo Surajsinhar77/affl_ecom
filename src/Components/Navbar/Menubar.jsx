ba133dcdba50afd4a12b99e870838f97d4d84193
Suraj Kumar <surajsinhaking77@gmail.com>
2023-09-30 02:47:21 +0530

    asjda

--- src/Components/Navbar/Menubar.jsx
+++ src/Components/Navbar/Menubar.jsx
@@ -4,15 +4,15 @@ import './MenuAndNav.css'; // Internal Like Css for NavPage only
 
 function Menubar() {
 
-    const menu = ['Profile', 'Service', 'Blog', 'About', 'contact']
+    const menu = ['Profile', 'Service', 'Blog', 'About', 'Contact']
+    const menu2 = ['Profile', 'Service', 'Blog', 'About', 'Contact', "Something ", "More item"]
     const [dw, setDW] = useState(false);
 
-    
     return (
         <>
             <div className="menubar bg-white w-full h-16 flex text-gray-500">
                 <ul className="menuIteams flex w-full justify-around items-center">
-                    <li> <Link>Home Accessory</Link> </li> 
+                    <li> <Link onClick={()=>setDW(!dw)}>Home Accessory</Link> </li> 
                     <li> <Link>Mobile</Link> </li> 
                     <li> <Link>Laptop</Link> </li> 
                     <li> <Link>Fashion</Link> </li> 
@@ -20,17 +20,24 @@ function Menubar() {
                     <li> <Link>Kitchen Accessory</Link> </li> 
                 </ul>
             </div>
-
-                {/* <div className="dropdown text-gray-800 bg-slate-50 p-2 absolute top-48 left-24">
+            {dw && (
+                <div className="dropdown flex text-gray-600 bg-white p-2 absolute top-48 left-24">
                     <ul>
                         {
                             menu.map((item)=>
-                                <li className='m-3 bg-blue-300 p-2'><Link >{item}</Link></li>
+                                <li className='m-1 bg-white-300 p-1 hover:bg-blue-400 hover:text-white'><Link >{item}</Link></li>
                             )
                         }
                     </ul>
-                </div> */}
-            
+                    <ul>
+                    {
+                            menu2.map((item)=>
+                                <li className='m-1 bg-white-300 p-1 hover:bg-blue-400 hover:text-white'><Link >{item}</Link></li>
+                            )
+                        }
+                    </ul>
+                </div>
+            )}
         </>
     )
 }
