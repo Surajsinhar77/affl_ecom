35bd03fe3ec3796ec9040cd2a3e29439ae5e407b
Divanshu Gupta <divanshugupta924@gmail.com>
2023-10-09 22:46:19 +0530

    a

--- src/Components/Product/Productdisp.jsx
+++ src/Components/Product/Productdisp.jsx
@@ -21,6 +21,9 @@ function Productdisp() {
 
     const [c1, setC1] = useState(true);
     const [c2, setC2] = useState(true);
+    const [c3, setC3] = useState(false);
+    const [s, setS] = useState([true,true,true,true,true]);
+    const [rating, setRating] = useState(0);
 
     const specs = [
         {
@@ -194,9 +197,10 @@ function Productdisp() {
                                 <div className=' flex w-fit justify-center items-center'>
                                     <div className=' text-3xl font-semibold text-center h-fit'>Ratings And Review</div>
                                 </div>
-                                <div className=' cursor-pointer px-10 py-5 border text-lg font-semibold shadow-md rounded-md'>Rate Product</div>
+                                <div className={`${ c3 ? "visible" : "hidden"} cursor-pointer px-10 py-5 border text-lg font-semibold shadow-md rounded-md`} onClick={()=>{setC3(false); setS([true, true, true, true, true])}}>Rate Product</div>
+                                <div className={`${ c3 ? "hidden" : "visible"} cursor-pointer px-10 py-5 border text-lg font-semibold shadow-md rounded-md`} onClick={()=>setC3(true)}>Submit</div>
                             </div>
-                            <div className=' flex justify-around w-full'>
+                            <div className={`flex justify-around w-full ${c3 ? "visible" : "hidden"}`}>
                                 <div className=' flex flex-col items-center justify-around w-1/3 p-10'>
                                     <div className=' text-[40px] font-semibold'>4.5★</div>
                                     <div className=' text-xl text-gray-700'>405 ratings and 58 reviews</div>
@@ -237,6 +241,23 @@ function Productdisp() {
                                         </div>
                                         <div>82</div>
                                     </div>
+                                </div>
+                            </div>
+                            <div className={` text-blue-700 flex p-10 justify-around ${c3 ? "hidden" : "visible"}`}>
+                                <div id='s1' className=' cursor-pointer text-[100px]' onClick={()=>{setS([false, true, true, true, true]); setRating(1)}}>
+                                    {s[0]? <div>&#9734;</div> : <div>&#9733;</div> }
+                                </div>
+                                <div id='s2' className=' cursor-pointer text-[100px]' onClick={()=>{setS([false, false, true, true, true]); setRating(2)}}>
+                                    {s[1]? <div>&#9734;</div> : <div>&#9733;</div> }
+                                </div>
+                                <div id='s3' className=' cursor-pointer text-[100px]' onClick={()=>{setS([false,false,false,true,true]); setRating(3)}}>
+                                    {s[2]? <div>&#9734;</div> : <div>&#9733;</div> }
+                                </div>
+                                <div id='s4' className=' cursor-pointer text-[100px]' onClick={()=>{setS([false,false,false,false,true]); setRating(4)}}>
+                                    {s[3]? <div>&#9734;</div> : <div>&#9733;</div> }
+                                </div>
+                                <div id='s5' className=' cursor-pointer text-[100px]' onClick={()=>{setS([false,false,false,false,false]); setRating(5)}}>
+                                    {s[4]? <div>&#9734;</div> : <div>&#9733;</div> }
                                 </div>
                             </div>
                         </div>
