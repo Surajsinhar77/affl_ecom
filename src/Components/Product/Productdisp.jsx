29e9813d6cf61e13f21d5c1fd10dbac31852d8e5
Divanshu Gupta <divanshugupta924@gmail.com>
2023-10-09 18:19:10 +0530

    v

--- src/Components/Product/Productdisp.jsx
+++ src/Components/Product/Productdisp.jsx
@@ -186,6 +186,63 @@ function Productdisp() {
                                 <button className='h-fit absolute bottom-0 cursor-pointer  px-3 rounded-full  bg-gray-700 text-white py-2 border border-gray-700 hover:bg-white hover:text-gray-700' onClick={()=>{setC1(false); setC2(false)}}>Show more</button>
                             </div>
                         </div>
+
+                        {/* Start new Components Rating From here  */}
+
+                        <div className=' border p-10 text-black rounded-md space-y-2'>
+                            <div className=' flex justify-between'>
+                                <div className=' flex w-fit justify-center items-center'>
+                                    <div className=' text-3xl font-semibold text-center h-fit'>Ratings And Review</div>
+                                </div>
+                                <div className=' cursor-pointer px-10 py-5 border text-lg font-semibold shadow-md rounded-md'>Rate Product</div>
+                            </div>
+                            <div className=' flex justify-around w-full'>
+                                <div className=' flex flex-col items-center justify-around w-1/3 p-10'>
+                                    <div className=' text-[40px] font-semibold'>4.5★</div>
+                                    <div className=' text-xl text-gray-700'>405 ratings and 58 reviews</div>
+                                </div>
+                                <div className=' w-2/3 flex flex-col items-center p-4 gap-3 text-lg'>
+                                    <div className=' flex w-full gap-2 align-middle'>
+                                        <div>5★</div>
+                                        <div className=' w-full h-full flex items-center justify-center'>
+                                            <div className=' w-full h-1 align-middle bg-green-700 rounded-lg'></div>
+                                        </div>
+                                        <div>265</div>
+                                    </div>
+                                    <div className=' flex w-full gap-2 align-middle'>
+                                        <div>4★</div>
+                                        <div className=' w-full h-full flex items-center justify-center'>
+                                            <div className=' w-full h-1 align-middle bg-green-700 rounded-lg'></div>
+                                        </div>
+                                        <div>123</div>
+                                    </div>
+                                    <div className=' flex w-full gap-2 align-middle'>
+                                        <div>3★</div>
+                                        <div className=' w-full h-full flex items-center justify-center'>
+                                            <div className=' w-full h-1 align-middle bg-green-700 rounded-lg'></div>
+                                        </div>
+                                        <div>87</div>
+                                    </div>
+                                    <div className=' flex w-full gap-2 align-middle'>
+                                        <div>2★</div>
+                                        <div className=' w-full h-full flex items-center justify-center'>
+                                            <div className=' w-full h-1 align-middle bg-orange-700 rounded-lg'></div>
+                                        </div>
+                                        <div>39</div>
+                                    </div>
+                                    <div className=' flex w-full gap-2 align-middle'>
+                                        <div>1★</div>
+                                        <div className=' w-full h-full flex items-center justify-center'>
+                                            <div className=' w-full h-1 align-middle bg-red-700 rounded-lg'></div>
+                                        </div>
+                                        <div>82</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/* End new Components Rating From here  */}
+
+
                         {/* Start new Components Reviews From here  */}
                         <Review/>
                         {/* End new Components Reviews From here  */}
