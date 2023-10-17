ba5070ea983a79afacca4534202988451b4692c3
Divanshu Gupta <divanshugupta924@gmail.com>
2023-10-17 12:36:29 +0530

    modification

--- frontend/src/Components/Profile/ProfilePage.jsx
+++ frontend/src/Components/Profile/ProfilePage.jsx
@@ -5,14 +5,14 @@ import Favourites from "./Favourites"
 
 function ProfilePage(){
     return(
-        <div className=" h-screen flex bg-slate-200 text-black">
-            <div className=" h-full p-2 pr-0 w-1/5">
+        <div className=" h-fit flex bg-slate-200 text-black">
+            <div className=" h-screen p-2 pr-0 w-1/5">
                 <div className=" h-full">
                     <ProfileNav/>
                 </div>
             </div>
             <div className=" w-4/5 p-2 h-full">
-                <div className=" h-full overflow-auto">
+                <div className=" h-full">
                     {/* <Profile/> */}
                     <Favourites/>
                 </div>
