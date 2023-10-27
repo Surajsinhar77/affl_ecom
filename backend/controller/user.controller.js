2cf9a426ff42df96c53804c118bd7093583a5096
Suraj Kumar <surajsinhaking77@gmail.com>
2023-10-28 01:38:16 +0530

    window from

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,9 +1,27 @@
 const userModel = require('../model/user.model');
+const bcrypt = require('bcrypt')
 
 const userRegister = async (req, res)=>{
-    const {name, email, password, } = req.body;
-    const getAllUserEmail = await userModel.find({email:userData.email});
-    
+    const {name, email, password } = req.body;
+
+    try{
+        const isUserExist = await userModel.findOne({email});
+        if(isUserExist){
+            return res.status(409).json({message:"User Already Exist", userExist: true});
+        }
+
+        const hashPassword =  await bcrypt.hash(password, 10);
+
+        
+        const user = new userModel({
+            fullname : name,
+            email, email,
+            password, hashPassword,
+            token : 
+        })
+    }catch(err){
+        return res.json(err);
+    }
 }
 
 module.exports = {
