dbd404f25f1943bab86cb8378b8ffcc92790ae60
suraj kumar <surajsinhakingr77>
2023-10-27 15:22:00 +0530

    done

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,7 +1,9 @@
 const userModel = require('../model/user.model');
 
-const userRegister = (req, res)=>{
-    return res.json({"message ": "Test api "})
+const userRegister = async (req, res)=>{
+    const userData = req.body;
+    const getAllUserEmail = await userModel.find({email:userData.email});
+    // return res.json({"message ": "Test api "})
 }
 
 module.exports = {
