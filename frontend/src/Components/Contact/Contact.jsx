0684e9bb2678d6826a48b7d1ef7141985e9d76a0
suraj kumar <surajsinhakingr77>
2023-12-12 00:36:59 +0530

    doing contact page

--- frontend/src/Components/Contact/Contact.jsx
+++ frontend/src/Components/Contact/Contact.jsx
@@ -6,7 +6,40 @@ function Contact() {
   return (
     // <ProfilePage/>
     <div>
-      <h1>Contact page</h1>
+      <div className="mainContactDiv bg-slate-200 rounded w-[50%] m-auto">
+        <div className="row my-5">
+          <h1 className='mainHeading font-bold text-4xl'>Contact Us</h1>
+        </div>
+        <div className="mainFormDiv uppercase">
+          <div className="row">
+            <div className="nameDiv">
+              <label htmlFor="fullname">Full Name</label>
+              <input type="text" name="fullname" />
+            </div>
+
+            <div className="email-and-phone row flex">
+              <div className="email row">
+                <label htmlFor="email">Email</label>
+                <input type="text" name="email" />
+              </div>
+              <div className="phoneno row">
+                <label htmlFor="phoneno">Phone</label>
+                <input type="number" name="phoneno" />
+              </div>
+            </div>
+
+            <div className="Motive">
+              <label htmlFor="Motive">Motive</label>
+              <input type="text" name="Motive" />
+            </div>
+
+            <div className="row message flex">
+              <label htmlFor="Message">Message</label>
+              <textarea name="message" cols="30" rows="10"></textarea>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
