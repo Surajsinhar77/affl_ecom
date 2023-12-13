40d143a1ad3ac06e1eb62507552996c90e221f4e
suraj kumar <surajsinhakingr77>
2023-12-13 17:56:55 +0530

    done the real change

--- backend/routes/items.routes.js
+++ backend/routes/items.routes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const upload = require('../middleware/multer.Uploader');
 
 router.post('/addProduct', upload.single('image') ,addItemsToInventary);
-// router.post('/uploadImage', uploadImageForInventory);
 router.post('/getDataFrom', getAllItemData);
 
 module.exports = router;
