09a6e56196fd475fa12eb5d4abf1b703279803cc
Suraj Kumar <surajsinhaking77@gmail.com>
2023-12-15 00:54:12 +0530

    now error is solved from the windows

--- backend/routes/items.routes.js
+++ backend/routes/items.routes.js
@@ -1,9 +1,9 @@
-const {addItemsToInventary, uploadImageForInventory, getAllItemData} = require('../controller/dashboard.controller');
+const {addItemsToInventary, uploadImageForInventory, getItems} = require('../controller/dashboard.controller');
 const express = require('express');
 const router = express.Router();
 const upload = require('../middleware/multer.Uploader');
 
 router.post('/addProduct', upload.single('image') ,addItemsToInventary);
-router.post('/getDataFrom', getAllItemData);
+router.post('/getDataFrom', getItems);
 
 module.exports = router;
