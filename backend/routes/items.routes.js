7f9e4a332a32f26fd038f02c64956d8790b8464c
divanshu Gupta <divanshugupta924@gmail.com>
2023-12-17 18:49:17 +0530

    done

--- backend/routes/items.routes.js
+++ backend/routes/items.routes.js
@@ -1,10 +1,17 @@
-const {addItemsToInventary, uploadImageForInventory, getItems} = require('../controller/dashboard.controller');
+const {
+    addItemsToInventary, 
+    uploadImageForInventory, 
+    getItems,
+    getData
+} = require('../controller/dashboard.controller');
+
 const express = require('express');
 const router = express.Router();
 const upload = require('../middleware/multer.Uploader');
 
 router.post('/addProduct', upload.single('image') ,addItemsToInventary);
 router.get('/getItems', getItems);
+router.get('/getData', getData);
 // router.post('/uploadImage', uploadImageForInventory);
 
 module.exports = router;
