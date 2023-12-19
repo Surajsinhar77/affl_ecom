a2369bc65e0e69e6700476463d872ea17c58fde5
Suraj Kumar <surajsinhaking77@gmail.com>
2023-12-19 22:48:42 +0530

    done some change for prod

--- client/src/Components/About/Teamcard.jsx
+++ client/src/Components/About/Teamcard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import team1 from '../../assets/Img/headphone.jpg';
 import {AiFillLinkedin, AiFillFacebook, AiFillGithub, AiFillYoutube, AiFillInstagram} from 'react-icons/ai';
 
-function TeamCard() {
+function TeamCard({data}) {
     return (
         <div className="memberOne teamCard flex flex-col items-center border border-red-500 py-5 px-3 rounded-lg bg-white">
             <img src={team1} alt="team member Image" className='w-48 h-48 object-cover rounded-full' />
-            <p className='my-5 text-3xl font-medium'> Suraj Kumar</p>
+            <p className='my-5 text-3xl font-medium'> {data.name} </p>
             <p className='text-blue-500 text-xl'>Developer</p>
             <p className='text-center my-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non tempore accusamus recusandae suscipit, libero consequatur aliquid repellendus voluptas explicabo itaque.</p>
             <div className="socalLinks flex justify-evenly w-full">
