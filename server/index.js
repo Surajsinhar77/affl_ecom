16a7e7d79498212bb09946929b6654625b917648
suraj kumar <surajsinhakingr77>
2024-01-08 01:26:40 +0530

    done

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const dashboardApiCall = require('./routes/items.routes');
 app.use('/auth', authUserRouter);
 app.use('/items',productIteamRoutes);
 app.use('/admin', adminRoutes);
-app.use('/dashboard', dashboardApiCall)
+app.use('/dashboard', userVerfication,dashboardApiCall)
 
 
 // Database Connection
