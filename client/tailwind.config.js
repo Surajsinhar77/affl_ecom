5c390c2801207ed7b30dfd16f3de7dbfb5eb07ce
divanshu Gupta <divanshugupta924@gmail.com>
2024-01-10 19:53:12 +0530

    done

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,6 +5,22 @@ export default {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '300px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '590px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '900px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
