1be4d8ae403a7bcd495c82cbf69bfd27dda7b46e
Suraj Kumar <surajsinhaking77@gmail.com>
2024-01-12 19:47:58 +0530

    done on the frontend for contact page

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,6 +3,10 @@ import axios from 'axios'
 const api = axios.create({
     baseURL: 'http://localhost:8000',
     timeout: 100000,
+    headers: {
+        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`, 
+        "Content-Type": "application/json",
+    },
 });
 
 export default api;
