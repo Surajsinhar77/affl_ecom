d04d90cf2900b5cb8ba07c97d6e6f388137e95a9
suraj kumar <surajsinhakingr77>
2024-01-14 04:23:33 +0530

    done

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,6 +1,8 @@
 const {userModel, userContactUsModel} = require("../model/user.model");
 const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer')
 const serviceAuth = require("../service/auth");
+const filedata = process.env.FILE_DATA; // from env 
 
 const userRegister = async (req, res) => {
     const { name, email, password } = req.body;
@@ -65,12 +67,64 @@ const userLogin = async(req, res)=>{
     }
 }
 
-const contactUsMail = (req, res) =>{
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 5000,
+    secure: false,
+    auth: {
+        user: filedata.myEmail,
+        pass: filedata.myEmailPass,
+    },
+});
+
+
+const contactUsMail = async(req, res) =>{
     const {name, email, subject , message} = req.body;
     try{
-        
-    }catch(err){
+        const mailOptions = {
+            from: email, // sender address
+            to: filedata.myEmail, // receiver email
+            subject: `${subject}`, // Subject line
+            text: `Hi there, you were emailed from ${name} <br/> ${message}`,
+            html: `<h1> ${email} is now Your Subscriber </h1>`,
+        }
 
+        const info = await transporter.sendMail(mailOptions)
+        if (info) {
+            console.log("done sending mail");
+
+
+
+            const thank = await transporter.sendMail(
+                {
+                    from: filedata.myEmail, // sender address
+                    to: email, // receiver email
+                    subject: "Thank You", // Subject line
+                    text: `Hi there, you were emailed from ${filedata.myEmail} `,
+                    html: `<h1> Thank yor From Subscribe ${email} </h1>`,
+                }
+            )
+
+            if (thank) {
+                req.session.additionalData = {
+                    response: { mailUser: true, ownMail: true, statusCode: 'ok', statusCode2: 'fail' },
+                    userMessage: 'Form submitted successfully!',
+                    ownMessage: 'Form submitted successfully!',
+                };
+            } else {
+                req.session.additionalData = {
+                    response: { mailUser: true, ownMail: false, statusCode1: 'ok', statusCode2: 'fail' },
+                    userMessage: 'Form submitted successfully!',
+                    ownMessage: 'Form submitted Unsuccessfully!',
+                };
+            }
+        }
+        return res.status(200).json({message:" Mail sucessfull sent"})
+    }catch(err){
+        console.log(err);
+        return res.status(404).json({message : err.message});
     }
 }
 
