4166d0d3b9bc3c00f54fab52fe7344173b466197
suraj kumar <surajsinhakingr77>
2024-01-14 05:11:45 +0530

    done few change

--- client/src/api/apiForAdmin.js
+++ client/src/api/apiForAdmin.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiForAdmin = axios.create({
-    baseURL: 'https://backend-nid8.onrender.com',
+    // baseURL: 'https://backend-nid8.onrender.com',
+    baseURL: 'http://localhost:8000',
     timeout: 100000,
     headers: {
         'Authorization': `Bearer ${localStorage.getItem('adminAccessToken')}`, 
