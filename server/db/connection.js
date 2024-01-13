e1245e43871f87fdb408a52066ce57495d03efef
Suraj Kumar <surajsinhaking77@gmail.com>
2024-01-14 02:01:20 +0530

    done

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,19 +1,7 @@
 const mongoose = require('mongoose');
 
-<<<<<<< HEAD
-const prod= true;
-
-function main(url) {
-    if(prod){
-        const url_prod = "mongodb+srv://learingdevelopment4:0aDP25RcCmnxDFdH@cluster0.nfd8eyo.mongodb.net/?retryWrites=true&w=majority"
-        return mongoose.connect(url_prod);
-    }
-    return mongoose.connect(url);
-
-=======
 function main(url) {
     return mongoose.connect(url);
->>>>>>> 518898d19968b22717f24d3365a9ea6739f44340
 }
 
 module.exports = {
