5e01da43484583d1aa1470f326267d35ae059c5d
suraj kumar <surajsinhakingr77>
2024-01-14 04:44:28 +0530

    done few change

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 const api = axios.create({
     baseURL: 'https://backend-nid8.onrender.com',
     timeout: 100000,
-
     headers: {
         'Authorization': `Bearer ${localStorage.getItem('accessToken')}`, 
         "Content-Type": "application/json",
