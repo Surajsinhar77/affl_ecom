dd429f3025f7a87a5d3e203472dd4f02d2625ab7
suraj kumar <surajsinhakingr77>
2024-01-14 05:56:15 +0530

    done

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,8 +1,11 @@
-const {userModel, userContactUsModel} = require("../model/user.model");
+const { userModel, userContactUsModel } = require("../model/user.model");
 const bcrypt = require('bcrypt');
 const nodemailer = require('nodemailer')
 const serviceAuth = require("../service/auth");
-const filedata = process.env.FILE_DATA; // from env 
+
+//Email data from Env file
+const myEmail = process.env.myEmail;
+const myEmailPass = process.env.myEmailPass;
 
 const userRegister = async (req, res) => {
     const { name, email, password } = req.body;
@@ -13,13 +16,13 @@ const userRegister = async (req, res) => {
         if (isUserExist) {
             return res.status(409).json({
                 error: 'User already registered',
-                message: 'The username or email is already taken', 
-                userExist: false 
+                message: 'The username or email is already taken',
+                userExist: false
             });
         }
 
         const hashPassword = await bcrypt.hash(password, 10);
-        const token = serviceAuth.setUserToken({ name , email });
+        const token = serviceAuth.setUserToken({ name, email });
 
         const user = new userModel({
             fullName: name,
@@ -30,42 +33,45 @@ const userRegister = async (req, res) => {
         });
 
         const result = await user.save();
-        res.cookie('uid', token, {httpOnly: true});
-        res.setHeader('Authorization' , `Bearer ${token}`);
-        return res.status(201).json({ message: "User is sucessfull SignUp", result});
+        res.cookie('uid', token, { httpOnly: true });
+        res.setHeader('Authorization', `Bearer ${token}`);
+        return res.status(201).json({ message: "User is sucessfull SignUp", result });
     } catch (err) {
         console.log("here is the errror ", err);
-        return res.status(404).json({message: err.message, err});
+        return res.status(404).json({ message: err.message, err });
     }
 };
 
-const userLogin = async(req, res)=>{
-    const {email, password} = req.body;
+const userLogin = async (req, res) => {
+    const { email, password } = req.body;
 
-    try{
-        const userExist = await userModel.findOne({email: email});
-        if(!userExist){
-            return res.json({message: "user doesn't Exist"});
+    try {
+        const userExist = await userModel.findOne({ email: email });
+        if (!userExist) {
+            return res.json({ message: "user doesn't Exist" });
         }
         const authToken = req.cookies?.uid;
 
-        if(!authToken){
+        if (!authToken) {
             const userExistInfo = await bcrypt.compare(password, userExist.password);
-            if(userExistInfo){
-                const token = serviceAuth.setUserToken({name:userExist.name,email});
-                res.cookie('uid', token, {httpOnly: true,});
-                return res.status(200).json({message:"You are SuccessFull logged in",
-                result: userExist, userExistInfo})
+            if (userExistInfo) {
+                const token = serviceAuth.setUserToken({ name: userExist.name, email });
+                res.cookie('uid', token, { httpOnly: true, });
+                return res.status(200).json({
+                    message: "You are SuccessFull logged in",
+                    result: userExist, userExistInfo
+                })
             }
-            return res.status(404).json({message:"Invalid Credintial", userExistInfo});
+            return res.status(404).json({ message: "Invalid Credintial", userExistInfo });
         }
-        
-        return res.json({message: "You are allow to login ",UserVerification});
-    }catch(err){
+
+        return res.json({ message: "You are allow to login ", UserVerification });
+    } catch (err) {
         console.log(err);
-        res.status(404).json({message:"You are getting Error",errorMsg:err});
+        res.status(404).json({ message: "You are getting Error", errorMsg: err });
     }
 }
+
 
 
 const transporter = nodemailer.createTransport({
@@ -74,57 +80,69 @@ const transporter = nodemailer.createTransport({
     port: 5000,
     secure: false,
     auth: {
-        user: filedata.myEmail,
-        pass: filedata.myEmailPass,
+        user: myEmail,
+        pass: myEmailPass,
     },
 });
 
 
-const contactUsMail = async(req, res) =>{
-    const {name, email, subject , message} = req.body;
-    try{
+const contactUsMail = async (req, res) => {
+
+    const { name, email, subject, message } = req.body.userFormData;
+    try {
         const mailOptions = {
             from: email, // sender address
-            to: filedata.myEmail, // receiver email
+            to: myEmail, // receiver email
             subject: `${subject}`, // Subject line
             text: `Hi there, you were emailed from ${name} <br/> ${message}`,
-            html: `<h1> ${email} is now Your Subscriber </h1>`,
+            html: `
+                <!DOCTYPE html>
+                <html>
+                <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <title>${subject}</title>
+                </head>
+                <body>
+                <div style="font-family: Arial, sans-serif; font-size: 16px; color: #333; max-width: 600px; margin: 0 auto;">
+                    <header style="background-color: #f5f5f5; padding: 20px;">
+                    <img src="https://your-logo-url" alt="Your Logo" style="width: 150px;">
+                    </header>
+                
+                    <h3 style="font-size: 24px; margin: 20px 0;">Hello ${name},</h3>
+                
+                    <p style="margin: 20px 0;">
+                        ${message}
+                    </p>
+                
+                    <a href="https://your-link" style="background-color: #007bff; color: #fff; padding: 10px 20px; text-decoration: none; border-radius: 5px; margin: 0 auto;">Click Here</a>
+                
+                    <footer style="background-color: #f5f5f5; padding: 20px; text-align: center;">
+                    <p>© 2023 Afflite MyAmazingStore</p>
+                    </footer>
+                </div>
+                </body>
+                </html>
+            `,
         }
 
         const info = await transporter.sendMail(mailOptions)
         if (info) {
             console.log("done sending mail");
-
-
-
             const thank = await transporter.sendMail(
                 {
-                    from: filedata.myEmail, // sender address
+                    from: myEmail, // sender address
                     to: email, // receiver email
                     subject: "Thank You", // Subject line
-                    text: `Hi there, you were emailed from ${filedata.myEmail} `,
+                    text: `Hi there, you were emailed from ${myEmail} `,
                     html: `<h1> Thank yor From Subscribe ${email} </h1>`,
                 }
             )
-
-            if (thank) {
-                req.session.additionalData = {
-                    response: { mailUser: true, ownMail: true, statusCode: 'ok', statusCode2: 'fail' },
-                    userMessage: 'Form submitted successfully!',
-                    ownMessage: 'Form submitted successfully!',
-                };
-            } else {
-                req.session.additionalData = {
-                    response: { mailUser: true, ownMail: false, statusCode1: 'ok', statusCode2: 'fail' },
-                    userMessage: 'Form submitted successfully!',
-                    ownMessage: 'Form submitted Unsuccessfully!',
-                };
-            }
         }
-        return res.status(200).json({message:" Mail sucessfull sent"})
-    }catch(err){
+        return res.status(200).json({ message: " Mail sucessfull sent" })
+    } catch (err) {
         console.log(err);
-        return res.status(404).json({message : err.message});
+        return res.status(404).json({ message: err.message });
     }
 }
 
