26a193c04d4d7902df0127967ce22e16a19bd737
suraj kumar <surajsinhakingr77>
2024-01-14 05:57:36 +0530

    done

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    // baseURL: 'https://backend-nid8.onrender.com',
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://backend-nid8.onrender.com',
+    // baseURL: 'http://localhost:8000',
     timeout: 100000,
     headers: {
         'Authorization': `Bearer ${localStorage.getItem('accessToken')}`, 
